9e01d287b7a33ef6c5da3d605805bfe765ee0824
marianneporter <mariannneporter17@gmail.com>
2021-07-16 20:43:46 +0100

    WIP horse list

--- src/app/_services/horse.service.ts
+++ src/app/_services/horse.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -11,10 +11,27 @@ export class HorseService {
 
     constructor(private http: HttpClient) { }
  
-    getHorses(): Observable<Horse[]> {
-        const url = environment.baseUrl + 'horses';
-        return this.http.get<Horse[]>(url);          
+    getHorses(filter='',
+              sortDirection='asc',
+              pageIndex =0,
+              pageSize=3): Observable<Horse[]> {
+
+            console.log('in gethorses of horses service');
+
+            let params = new HttpParams();
+            params = params.append("pageIndex", pageIndex.toString());
+            params = params.append("pageSize", pageSize.toString());        
+
+            const url = environment.baseUrl + 'horses';
+
+            return this.http.get<Horse[]>(url, { params: params });
+
     }   
+
+    getHorseCount() {
+        const url = environment.baseUrl + 'horses/count';
+        return this.http.get<number>(url);              
+    }  
 
   
   
