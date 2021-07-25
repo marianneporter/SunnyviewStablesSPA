7bc7599e717c92d081eebcebc35d87fabe36764d
marianneporter <mariannneporter17@gmail.com>
2021-07-25 16:15:57 +0100

    WIP css-grid-version

--- src/app/horse/horse-list-element/horse-list-element.component.ts
+++ src/app/horse/horse-list-element/horse-list-element.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Horse } from 'src/app/_models/horse';
 
 @Component({
   selector: 'app-horse-list-element',
   templateUrl: './horse-list-element.component.html',
-  styleUrls: ['./horse-list-element.component.scss']
+  styleUrls: ['./horse-list-element.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HorseListElementComponent implements OnInit {
 
-    @Input() horses$: Observable<Horse[]>;
-
-    displayedColumns: string[] = ['imageUrl', 'name', 'height', 'colour', 'sex', 'dob', 'owners'];
+    @Input() horses$: Observable<Horse[]>;  
 
     constructor() { }
 
